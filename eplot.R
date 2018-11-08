d21aaf9b4519a2d2e8fc46a98a8facc9c66b5572
Ott Toomet <otoomet@uw.edu>
2018-11-07 17:11:24 -0800

    labels

--- eplot.R
+++ eplot.R
@@ -5,5 +5,7 @@ data <- read.delim("data/uw-elevations.csv.bz2")
 ggplot(data) +
    geom_point(aes(lon, lat, col=elevation)) +
    scale_color_gradientn(colors=terrain.colors(10)) +
-   coord_quickmap()
+   coord_quickmap() +
+   labs(x="longitude (deg)", color="elevation (m)")
+
 
