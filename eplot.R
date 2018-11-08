878b0511cc2664c537ff3ba6b9a15268d16b6d99
Ott Toomet <otoomet@uw.edu>
2018-11-07 17:09:48 -0800

    map coords

--- eplot.R
+++ eplot.R
@@ -4,5 +4,6 @@ data <- read.delim("data/uw-elevations.csv.bz2")
 
 ggplot(data) +
    geom_point(aes(lon, lat, col=elevation)) +
-   scale_color_gradientn(colors=terrain.colors(10))
+   scale_color_gradientn(colors=terrain.colors(10)) +
+   coord_quickmap()
 
