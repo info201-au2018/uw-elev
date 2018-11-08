ed55e8e003f29c2531168f6e7ec9165a0bf58bad
Ott Toomet <otoomet@uw.edu>
2018-11-07 17:07:20 -0800

    terrain colors

--- eplot.R
+++ eplot.R
@@ -3,4 +3,6 @@ library(ggplot2)
 data <- read.delim("data/uw-elevations.csv.bz2")
 
 ggplot(data) +
-   geom_point(aes(lon, lat, col=elevation))
+   geom_point(aes(lon, lat, col=elevation)) +
+   scale_color_gradientn(colors=terrain.colors(10))
+
